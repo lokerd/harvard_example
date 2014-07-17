3528adfffecb58e39ff4a0717204966e0aa9dd40
lokerd@ <lokerd@amazon.com>
2014-07-17 16:58:02 +1000

    Final

--- recipes/setprivateip.rb
+++ recipes/setprivateip.rb
@@ -18,10 +18,9 @@ puts node[:opsworks][:instance][:hostname]
 
 ruby_block "Set Private IP" do
     block do
-    output = ec2.client.describe_network_interfaces( { :filters => [{ name: "attachment.instance-id", values: [node[:opsworks][:instance][:aws_instance_id]]}] })[:network_interface_set]
+    output = ec2.client.describe_network_interfaces( { :filters => [{ "name" => "attachment.instance-id", "values" => [node[:opsworks][:instance][:aws_instance_id]]}] })[:network_interface_set]
     eni = output[0][:network_interface_id]
     ec2.client.assign_private_ip_addresses({:allow_reassignment => true, :network_interface_id => eni, :private_ip_addresses => [ipconfiguration[node[:opsworks][:instance][:hostname]]]})
     end
     action :create
 end 
-
