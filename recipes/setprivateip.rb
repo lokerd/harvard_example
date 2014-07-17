e2ddc83b4e36ef2825718556eba1e8ef07054184
lokerd@ <lokerd@amazon.com>
2014-07-17 12:07:38 +1000

    Testing

--- recipes/setprivateip.rb
+++ recipes/setprivateip.rb
@@ -2,6 +2,10 @@
 
 proxynodes = node[:opsworks][:layers][:lb][:instances]
 
+gem_package "aws-sdk" do
+    action :install
+end
+
 ruby_block "Set Private IP" do
     block do
        for proxynode in proxynodes do
